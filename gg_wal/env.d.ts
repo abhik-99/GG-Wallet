4a6404ba0189fcb37447727ca5e8b8cd404ad5a8
abhik-bits <127435906+abhik-bits@users.noreply.github.com>
2023-12-09 12:50:18 +0530

    Working on profile verification using Anon AADHAR. Need to create a backend.

--- gg_wal/env.d.ts
+++ gg_wal/env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_W3A_CLIENT_ID: string
+  readonly VITE_APP_ANON_AADHAR_APP_ID: string
   // more env variables...
 }
 
--- gg_wal/src/pages/profile/page.tsx
+++ gg_wal/src/pages/profile/page.tsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import useTheme from "@mui/material/styles/useTheme";
+import PasswordIcon from "@mui/icons-material/Password";
+import React from "react";
+import { useAccount } from "wagmi";
+import Box from "@mui/material/Box";
+import { IconButton } from "@mui/material";
 
 export const Profile = () => {
+  const { address } = useAccount();
+  const theme = useTheme();
   return (
-    <div>Profile</div>
-  )
-}
+    <Container>
+      <Typography variant="h2">Your Profile</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4}>
+          <Paper
+            sx={{
+              p: 2,
+              // background: "transparent",
+              border: `solid ${theme.palette.grey[700]} 3px`,
+              mt: 2,
+            }}
+          >
+            <Typography variant="h4">Yourself:</Typography>
+            <Typography>Address: </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {address}
+            </Typography>
+            <Divider sx={{ my: 2 }} />
+            <Typography>Verified:</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="body2" color="red">
+                Unverified
+              </Typography>
+              <IconButton >
+                <PasswordIcon color="success"/>
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
