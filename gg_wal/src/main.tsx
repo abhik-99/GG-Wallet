2f0d87ba2ce8a1aa0416903026bfd78ca76df87b
abhik-bits <127435906+abhik-bits@users.noreply.github.com>
2023-12-09 10:50:12 +0530

    Bug fix created as baseline.

--- gg_wal/src/main.tsx
+++ gg_wal/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import App from "./App.tsx";
 import { theme } from "./assets/themes/theme.ts";
 
 import {
